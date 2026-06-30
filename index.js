b80cef7aac979de33b00e1829f8b022feb35c53b
Joni Honkanen <joni.honkanen@tuni.fi>
2026-06-30 22:44:03 +0300

    Add frontend as static

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
 //Use this for static Frontend build file
